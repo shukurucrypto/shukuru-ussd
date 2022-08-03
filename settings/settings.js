fb33d10d4046b107e94a6ff218ff4a947700c418
JovanMwesigwa <jovanmwesigwa79@gmail.com>
2022-08-03 12:16:34 +0300

    Switched to rinkeby

--- settings/settings.js
+++ settings/settings.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const { HARDHAT_RPC, RINKEBY_RPC_URL, ROPSTEN_URL, POLYGON_MUMBAI, NETWORK } =
   process.env
 
-const providerRPCURL = HARDHAT_RPC
+const providerRPCURL = RINKEBY_RPC_URL
 const networkServer = 1
 
 module.exports = {
