6610cfef32e29229aa8a57a400d22e011efafc65
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-10-15 16:32:50 +0300

    Logged binance pricers

--- utils/currencyConvertor.js
+++ utils/currencyConvertor.js
@@ -3,32 +3,28 @@ const CC = require('currency-converter-lt')
 
 const currencyConvertor = async (amount, currencyFrom, currencyTo) => {
   // let convertedAmount = 0
-  // try {
-  if (Number(amount) <= 0) {
-    return Number(amount)
+  try {
+    if (Number(amount) <= 0) {
+      return Number(amount)
+    }
+
+    let currencyConverter = new CC({
+      from: currencyFrom,
+      to: currencyTo,
+      amount: Number(amount),
+    })
+
+    let convertedAmount = await currencyConverter.convert(Number(amount))
+
+    // await currencyConverter.convert(Number(amount)).then((response) => {
+    //   convertedAmount = response
+    // })
+
+    return convertedAmount.toString()
+  } catch (error) {
+    console.log(error.response)
+    return error.response
   }
-
-  let currencyConverter = new CC({
-    from: currencyFrom,
-    to: currencyTo,
-    amount: Number(amount),
-  })
-
-  let convertedAmount = await currencyConverter.convert(Number(amount))
-
-  console.log('1 ====================================')
-  console.log(convertedAmount)
-  console.log('====================================')
-
-  // await currencyConverter.convert(Number(amount)).then((response) => {
-  //   convertedAmount = response
-  // })
-
-  return convertedAmount.toString()
-  // } catch (error) {
-  // console.log(error.response)
-  // return error.response
-  // }
 }
 
 const satsConvertor = async (sats, userCurrency) => {
@@ -45,9 +41,9 @@ const satsConvertor = async (sats, userCurrency) => {
     convertedSats = await currencyConvertor(satsAmount, 'USD', userCurrency)
   }
 
-  console.log('2 ====================================')
-  console.log(convertedSats)
-  console.log('====================================')
+  // console.log('2 ====================================')
+  // console.log(convertedSats)
+  // console.log('====================================')
 
   return Number(convertedSats)
   // } catch (error) {
@@ -59,32 +55,36 @@ const satsConvertor = async (sats, userCurrency) => {
 }
 
 const getSatsToUSD = async (satsAmount) => {
-  try {
-    // Step 1: Get the latest BTC price in USDT
-    const response = await axios.get(
-      'https://api.binance.com/api/v3/ticker/price',
-      {
-        params: { symbol: 'BTCUSDT' },
-      }
-    )
-
-    if (response.data && response.data.price) {
-      const btcToUSDPrice = parseFloat(response.data.price)
-
-      // Step 2: Convert SATs to BTC
-      const btcAmount = satsAmount / 100000000 // 100,000,000 SATs in 1 BTC
-
-      // Step 3: Calculate the USD value
-      const usdAmount = btcAmount * btcToUSDPrice
-      return usdAmount
-    } else {
-      // throw new Error('Unable to fetch BTC price from Binance API')
-      return 'Unable to fetch BTC price from Binance API'
+  // try {
+  // Step 1: Get the latest BTC price in USDT
+  const response = await axios.get(
+    'https://api.binance.com/api/v3/ticker/price',
+    {
+      params: { symbol: 'BTCUSDT' },
     }
-  } catch (error) {
-    // new Error('Error converting SATs to USD: ' + error.message)
-    return 'Error converting SATs to USD: ' + error.message
+  )
+
+  console.log('====================================')
+  console.log(response.data)
+  console.log('====================================')
+
+  if (response.data && response.data.price) {
+    const btcToUSDPrice = parseFloat(response.data.price)
+
+    // Step 2: Convert SATs to BTC
+    const btcAmount = satsAmount / 100000000 // 100,000,000 SATs in 1 BTC
+
+    // Step 3: Calculate the USD value
+    const usdAmount = btcAmount * btcToUSDPrice
+    return usdAmount
+  } else {
+    // throw new Error('Unable to fetch BTC price from Binance API')
+    return 'Unable to fetch BTC price from Binance API'
   }
+  // } catch (error) {
+  //   // new Error('Error converting SATs to USD: ' + error.message)
+  //   return 'Error converting SATs to USD: ' + error.message
+  // }
 }
 
 module.exports = {
