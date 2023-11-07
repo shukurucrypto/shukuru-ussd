0748aedbc1d980d34e23f407fa0ba3cf26b2738d
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-11-07 15:38:25 +0300

    Refactored into coingeko api

---
+++ models/UserOtp.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const UserOtpSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const UserOtp = mongoose.model('UserOtp', UserOtpSchema)
+module.exports = UserOtp
