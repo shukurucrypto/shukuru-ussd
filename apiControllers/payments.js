907ceb1e9c825563ed6ec867c2292e46c806f80e
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2024-02-10 20:42:35 +0300

    Refactor lookupAPIInvoiceStatus function to use hexadecimal string for r_hash

--- apiControllers/payments.js
+++ apiControllers/payments.js
@@ -156,20 +156,16 @@ const lookupAPIInvoiceStatus = async (req, res) => {
     const { r_hash } = req.body
 
     // Re-decode the invoice hash
-    // const binaryRHash = Buffer.from(r_hash, 'base64')
+    const binaryRHash = Buffer.from(r_hash, 'base64')
 
     // // Convert binary data to hexadecimal string
-    // const hexRHash = binaryRHash.toString('hex')
+    const hexRHash = binaryRHash.toString('hex')
 
     const data = {
-      r_hash: r_hash,
+      r_hash: hexRHash,
     }
 
     const boltInstance = await boltGETRequest(data, req.bolt, '/invoice/lookup')
-
-    console.log('====================================')
-    console.log('bolt', boltInstance)
-    console.log('====================================')
 
     if (!boltInstance.success)
       return res
@@ -181,9 +177,6 @@ const lookupAPIInvoiceStatus = async (req, res) => {
       data: boltInstance,
     })
   } catch (error) {
-    console.log('====================================')
-    console.log(error)
-    console.log('====================================')
     return res.status(500).json(error.message)
   }
 }
