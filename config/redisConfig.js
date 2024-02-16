665736cb99073c6786ad3b0236f321fa31e11727
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2024-02-16 11:12:32 +0300

    Refactor Redis connection in redisConfig.js

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -4,13 +4,13 @@ dotenv.config()
 const redis = require('redis')
 
 // Connect to Redis
-// const redisClient = redis.createClient({
-//   host: process.env.REDIS_HOST,
-//   port: process.env.REDIS_PORT,
-//   password: process.env.REDIS_PASSWORD,
-//   url: process.env.REDIS_URL,
-// })
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+  url: process.env.REDIS_URL,
+})
 
-const redisClient = redis.createClient()
+// const redisClient = redis.createClient()
 
 module.exports = redisClient
